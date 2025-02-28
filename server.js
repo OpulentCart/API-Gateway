4d731fe8680c6caf366cd6cbd2b8643e95410230
Vedang <vedang.wappnet@gmail.com>
2025-02-28 18:30:12 +0530

    added path-routes

--- server.js
+++ server.js
@@ -6,29 +6,62 @@ dotenv.config(); // Load environment variables
 
 const app = express();
 
-// Define microservices
-const services = {
-  auth: "http://localhost:8000",
-  //order: "http://localhost:5002",
-  //payment: "http://localhost:5003",
-  cart: "http://localhost:5007",  // ✅ Cart service running on 5007
-  vendor: "http://localhost:5005",
-  product: "http://localhost:5006",
-};
-
 app.use(
     "/api/cart-items",
     createProxyMiddleware({
-      target: "http://localhost:5007/cart-items", // Backend service for chat
+      target: "http://localhost:5007/cart-items", // Backend service for cart
       changeOrigin: true,
       pathRewrite: {
-        "^/api": "", // Remove '/api' prefix before forwarding to chat backend
+        "^/api": "", // Remove '/api' prefix 
       },
       onProxyReq: (proxyReq, req, res) => {
         console.log(`Proxying /api/cart-items request to backend at port 8002`);
       },
     })
   );
+
+  app.use(
+    "/api/vendors",
+    createProxyMiddleware({
+      target: "http://localhost:5002/vendors", // Backend service for vendors
+      changeOrigin: true,
+      pathRewrite: {
+        "^/api": "", // Remove '/api' prefix 
+      },
+      onProxyReq: (proxyReq, req, res) => {
+        console.log(`Proxying /api/vendors request to backend at port 8002`);
+      },
+    })
+  );
+
+  app.use(
+    "/api/products",
+    createProxyMiddleware({
+      target: "http://localhost:5004/products", // Backend service for product
+      changeOrigin: true,
+      pathRewrite: {
+        "^/api": "", // Remove '/api' prefix 
+      },
+      onProxyReq: (proxyReq, req, res) => {
+        console.log(`Proxying /api/products request to backend at port 8002`);
+      },
+    })
+  );
+
+  app.use(
+    "/api/notifications",
+    createProxyMiddleware({
+      target: "http://localhost:5008/notifications", // Backend service for notifications
+      changeOrigin: true,
+      pathRewrite: {
+        "^/api": "", // Remove '/api' prefix 
+      },
+      onProxyReq: (proxyReq, req, res) => {
+        console.log(`Proxying /api/notifications request to backend at port 8002`);
+      },
+    })
+  );
+
 // Start the API Gateway
 const port = process.env.API_GATEWAY_PORT || 8000;
 app.listen(port, () => {
